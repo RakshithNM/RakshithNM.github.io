73b6d1a0f2b79f4c073746b103721b740868fbfe
RakshithNM <rakshith_bellare@outlook.com>
2018-02-16 23:44:25 +0530

    Cleaned up the files

--- script.js
+++
@@ -1,9 +0,0 @@
-$( document ).ready(function(){
-	$("#pageOne-name").hide();
-	$("#pageOne-name").fadeIn(4000);
-});
-
-$( "#click-icon" ).on( "click", function() {
-	$("#projectPage-tile1").fadeIn(4000); 
-});
-
