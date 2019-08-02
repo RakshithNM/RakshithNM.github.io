dd2f89264881485b94896adb4f7528cd88599cc9
rakshithbellare <rb@rakshithbellares-MacBook-Pro.local>
2019-08-02 23:10:56 +0530

    Made the attractor follow the mouse pointer

--- src/sketch.js
+++ src/sketch.js
@@ -4,18 +4,16 @@ var canvas;
 
 function setup() {
   canvas = createCanvas(windowWidth, windowHeight);
-  console.log(canvas);
-  console.log("loggint");
   canvas.position(0, 0);
   canvas.style('z-index', '-1');
-  for(var i = 0; i < 1000; ++i) {
+  for(var i = 0; i < 100; ++i) {
     particles.push(new Particle(random(width), random(height), random(2)));
   }
-  attractor = new Attractor(0, height/2);
 }
 
 
 function draw() {
+  attractor = new Attractor(mouseX, mouseY);
   background(256, 256, 256);
   for(var i = 0; i < particles.length; ++i) {
     var attraction = attractor.calculateAttraction(particles[i]);
