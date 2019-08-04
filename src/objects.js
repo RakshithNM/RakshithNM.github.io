f491d34e03594613b99f1cf44ba10def82ec4df2
rakshithbellare <rb@rakshithbellares-MacBook-Pro.local>
2019-08-04 14:26:58 +0530

    New particle animation

--- src/objects.js
+++ src/objects.js
@@ -1,68 +1,54 @@
-var Particle = function(inX, inY, inMass) {
-  this.position = createVector(inX, inY);
-  this.velocity = createVector(0, 0);
-  this.acceleration = createVector(0, 0);
-  this.mass = inMass;
+var speed = 0.1;
+var maxSpeed = 2;
+
+var Particle = function(inPosition, inRadius, inMouseRadius) {
+  this.position = inPosition; 
+  this.velocity = createVector(random(-1, 1), random(-1, -1));
+  this.radius = inRadius;
+  this.mouseRadius = inMouseRadius;
 }
 
-Particle.prototype.update = function() {
-  this.velocity.add(this.acceleration);
+Particle.prototype.update = function(inParticles, index) {
+  // Map the position to be from 0 to 255
+  var h = map(this.position.x, 0, height, 0, 255);
+  // Make the particle move
   this.position.add(this.velocity);
 
-  this.acceleration.set(0, 0);
-}
+  // when the particle goes out of bounds
+  if(this.position.x < -10) this.position.x = width;
+  if(this.position.x > width + 10) this.position.x = 0;
+  if(this.position.y < -10) this.position.y = height;
+  if(this.position.y > height + 10) this.position.y = 0;
 
-Particle.prototype.display = function() {
-  fill(244);
-  ellipse(this.position.x, this.position.y, this.mass * 10, this.mass * 10);
-}
+  // constrain the speed
+  this.velocity.x = constrain(this.velocity.x + random(-speed, speed), -maxSpeed, maxSpeed);
+  this.velocity.y = constrain(this.velocity.y + random(-speed, speed), -maxSpeed, maxSpeed);
 
-Particle.prototype.applyForce = function(inForce) {
-  const f = inForce.copy();
-  f.div(this.mass);
-  this.acceleration.add(f);
-}
+  for(var j = index + 1; j < inParticles.length; ++j) {
+    var angle = atan2(this.position.y -  inParticles[j].position.y, this.position.x - inParticles[j].position.x);
+    var dist = this.position.dist(inParticles[j].position);
 
-Particle.prototype.edges = function() {
-  if(this.position.y > height) {
-    this.velocity.y *= -1;
-    this.position.y = height;
+    if(dist < this.radius) {
+      stroke(h, 255, map(dist, 0, this.radius, 255, 0));
+      strokeWeight(map(dist, 0, this.radius, 2, 0));
+      line(this.position.x, this.position.y, inParticles[j].position.x, inParticles[j].position.y);
+      var force = map(dist, 0, this.radius, 5, 0);
+      this.velocity.x += force * cos(angle);
+      this.velocity.y += force * sin(angle);
+    }
   }
-  if(this.position.y < 0) {
-    this.velocity.y *= -1;
-    this.position.y = 0
+
+  var angle = atan2(this.position.y - mouseY, this.position.x - mouseX);
+  var dist = this.position.dist(createVector(mouseX, mouseY));
+
+  if(dist < this.radius) {
+    var force = map(dist, 0, this.radius, 50, 0);
+    this.velocity.x += force * cos(angle);
+    this.velocity.y += force * sin(angle);
   }
-  if(this.position.x > width) {
-    this.velocity.x *= -1;
-    this.position.x = width;
-  }
-  if(this.position.x < 0) {
-    this.velocity.x *= -1;
-    this.position.x = 0;
-  }
+
+  noStroke();
+  fill(h, 255, 255);
+  ellipse(mouseX, mouseY, 30, 30);
+  ellipse(this.position.x, this.position.y, random(5), random(5));
 }
-
-var Attractor = function(inX, inY) {
-  this.position = createVector(inX, inY);
-  this.mass = 20;
-  this.G = 1
-}
-
-Attractor.prototype.display = function() {
-  stroke(126);
-  ellipse(this.position.x, this.position.y, 50, 50);
-}
-
-
-Attractor.prototype.calculateAttraction = function(inParticle) {
-  var force = p5.Vector.sub(this.position,inParticle.position);  
-  var distance = force.mag();
-  
-  distance = constrain(distance, 5, 25);
-  force.normalize();
-  var strength = (this.G * this.mass * inParticle.mass) / (distance * distance); 
-
-  force.mult(strength);
-  return force;
-}
-
--- src/sketch.js
+++ src/sketch.js
@@ -1,28 +1,23 @@
-var particles = [];
-var attractor;
 var canvas;
+var particle;
+var numberOfParticles = 200;
+var particles = new Array(numberOfParticles);
 
 function setup() {
   canvas = createCanvas(windowWidth, windowHeight);
   canvas.position(0, 0);
   canvas.style('z-index', '-1');
-  for(var i = 0; i < 100; ++i) {
-    particles.push(new Particle(random(width), random(height), random(2)));
+  colorMode(HSB);
+  for(var i = 0; i < numberOfParticles; ++i) {
+    particles[i] = new Particle(createVector(random(width), random(height)), 75, 100);
   }
+  stroke(255);
 }
 
 
 function draw() {
-  attractor = new Attractor(mouseX, mouseY);
-  background(256, 256, 256);
+  background(244)
   for(var i = 0; i < particles.length; ++i) {
-    var attraction = attractor.calculateAttraction(particles[i]);
-
-    particles[i].applyForce(attraction);
-
-    //particle.edges();
-    particles[i].update();
-    particles[i].display();
+    particles[i].update(particles, i);
   }
-  attractor.display();
 }
